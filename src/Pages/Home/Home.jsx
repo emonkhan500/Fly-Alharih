fcf9ea2748a6809fdff92f1e443fea828b4012a2
emonkhan500 <khdilo47@gmail.com>
2025-08-05 12:00:50 +0600

    airlines card design complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PaymentMethod from '../../Components/PaymentMethod';
+import Airlines from '../../Components/Airlines';
 
 const Home = () => {
     return (
         <div>
+            <Airlines></Airlines>
             <PaymentMethod></PaymentMethod>
         </div>
     );
--- src/Shared/Heading.jsx
+++ src/Shared/Heading.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Heading = ({title, des}) => {
     return (
-        <div className="mx-auto text-center md:w-3/6 lg:w-2/6 ">
-        <h1 className="text-3xl font-semibold wi">{title}</h1>
-        <p className="text-gray-600 mt-3  text-sm"> 
+        <div className="mx-auto text-center md:w-3/6 lg:w-2/6 px-3">
+        <h1 className="text-3xl font-semibold text-start md:text-center">{title}</h1>
+        <p className="text-gray-600 mt-3 text-start md:text-center text-sm"> 
           {des}
         </p>
       </div>

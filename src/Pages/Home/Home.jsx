36facf5f652a2caeb42ba81466060a427399f856
emonkhan500 <khdilo47@gmail.com>
2025-08-05 21:14:59 +0600

    destination slider added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PaymentMethod from '../../Components/PaymentMethod';
 import Airlines from '../../Components/Airlines';
+import Destination from '../../Components/Destination';
 
 const Home = () => {
     return (
         <div>
+            <Destination></Destination>
             <Airlines></Airlines>
             <PaymentMethod></PaymentMethod>
         </div>
--- src/Shared/Heading.jsx
+++ src/Shared/Heading.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Heading = ({title, des}) => {
     return (
         <div className="mx-auto text-center md:w-3/6 lg:w-2/6 px-3">
-        <h1 className="text-3xl font-semibold text-start md:text-center">{title}</h1>
+        <h1 className="text-2xl text-black md:text-3xl font-semibold text-start md:text-center">{title}</h1>
         <p className="text-gray-600 mt-3 text-start md:text-center text-sm"> 
           {des}
         </p>

c54a2941c52c4b2bef3ecdc22bcaf48a63cd1b06
sobuj54 <sobujahmed124@gmail.com>
2025-08-05 19:22:37 +0600

    chaged in layout and index.css

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,18 +1,15 @@
-import { Outlet } from 'react-router-dom';
-import Nav from '../Shared/Nav';
-import Footer from '../Shared/Footer';
+import { Outlet } from "react-router-dom";
+import Nav from "../Shared/Nav";
+import Footer from "../Shared/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <Nav></Nav>
-           <div className=''>
-           <Outlet></Outlet>
-           </div>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
-
 
 export default Main;
